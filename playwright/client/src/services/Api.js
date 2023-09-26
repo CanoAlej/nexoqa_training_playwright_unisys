894afeef291636fb02d22b0a44cd0e0950bde5ff
EU\CanoAlej <alejandro.cano@unisys.com>
2023-09-26 17:24:41 +0200

    Prueba PlayWright

--- playwright/client/src/services/Api.js
+++ playwright/client/src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`,
+    baseURL: `http://192.168.0.57:8081/`,
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
